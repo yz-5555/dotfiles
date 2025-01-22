575118f6bdcd4d49314ed0dfb4481633a1eaf3d3
yz <yunjae00734@gmail.com>
2025-01-23 01:49:07 +0900

    update.ps1

---
+++ update.ps1
@@ -0,0 +1,15 @@
+function Dotfile ($source, $target) {
+	# Remove old target
+	if (Test-Path -Path $target) {
+		Remove-Item -Path $target -Recurse -Force
+		Write-Host "Removed $target"
+	}
+	# Copy & Paste
+	if (Test-Path -Path $source) {
+		Copy-Item -Path $source -Destination $target -Recurse -Force
+		Write-Host "Copied $source to $target"
+	}
+}
+
+Dotfile -source "C:\Users\yz-win\.wezterm.lua" -target "wezterm\.wezterm.lua"
+Dotfile -source "C:\Users\yz-win\AppData\Local\nvim" -target "nvim"

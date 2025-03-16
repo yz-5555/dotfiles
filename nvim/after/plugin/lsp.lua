6a33b1a1492256138fa79c3429e9333f30d51ce3
yz <yunjae00734@gmail.com>
2025-03-16 20:56:36 +0900

    20250316 Everything fixed for now. I still don't understand what FzfLua grep_quickfix does.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,6 +4,5 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		client.server_capabilities.semanticTokensProvider = nil
 		client.server_capabilities.documentHighlightProvider = false
-
 	end,
 })
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -19,20 +19,11 @@ return {
 						size = 0.3,
 					},
 				},
+
 			},
 		},
 		cmd = "Trouble",
 		keys = {
-			{
-				"<leader>xx",
-				"<cmd>Trouble diagnostics toggle<cr>",
-				desc = "Diagnostics (Trouble)",
-			},
-			{
-				"<leader>xX",
-				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-				desc = "Buffer Diagnostics (Trouble)",
-			},
 			{
 				"<leader>cs",
 				"<cmd>Trouble symbols toggle focus=false<cr>",
@@ -42,16 +33,6 @@ return {
 				"<leader>cl",
 				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
 				desc = "LSP Definitions / references / ... (Trouble)",
-			},
-			{
-				"<leader>xL",
-				"<cmd>Trouble loclist toggle<cr>",
-				desc = "Location List (Trouble)",
-			},
-			{
-				"<leader>xQ",
-				"<cmd>Trouble qflist toggle<cr>",
-				desc = "Quickfix List (Trouble)",
 			},
 		},
 	},

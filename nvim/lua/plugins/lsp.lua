3f7c3a080387a3b60cb76618813df1b1d5bc0c13
yz <yunjae00734@gmail.com>
2025-03-27 17:08:07 +0900

    20250327 blink.cmp instead of nvim-cmp / indent-blankline for better indenting. Still nvim-lspconfig

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,8 +15,9 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,

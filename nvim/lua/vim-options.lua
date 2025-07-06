db3a1130afb355d55893b97c849b09fc4cb788d4
yz <yunjae00734@gmail.com>
2025-07-07 00:02:10 +0900

    20250707 neovim yank feature added

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -9,7 +9,7 @@ vim.g.mapleader = " "
 -- Set default terminal to powershell
 vim.o.shell = "powershell"
 vim.o.shellcmdflag =
-	"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellquote = ""
@@ -17,3 +17,8 @@ vim.o.shellxquote = ""
 
 -- Set border
 vim.o.winborder = "rounded"
+
+-- yank
+vim.api.nvim_set_option_value("clipboard", "unnamedplus", {})
+vim.keymap.set({ "n", "x" }, "cp", '"+y')
+vim.keymap.set({ "n", "x" }, "cv", '"+p')

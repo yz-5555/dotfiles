874ab437d472d50f436e62e06fb8858089f3e39d
yz-5555 <yz5555@proton.me>
2026-01-20 23:48:55 +0900

    fed43-wsl

---
+++ fed43-wsl/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "master",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "lua", "python", "javascript", "c", "cpp", "rust", "zig" },
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = { "rust" },
+				},
+				additional_vim_regex_highlighting = false,
+				indent = {
+					enable = true,
+					disable = { "rust", "c" },
+				},
+			})
+		end,
+	},
+}
---
+++ fed43-wsl/nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("set shiftwidth=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set number")
+
+vim.o.termguicolors = true
+vim.g.mapleader = " "
+
+-- Set border
+vim.o.winborder = "rounded"

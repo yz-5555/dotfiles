4ebc474c25a86561012082b3ab9e23a5a4b2d872
yz-5555 <yz5555@proton.me>
2026-04-08 21:23:08 +0900

    [fix(wip)] new treesitter config for 0.12

--- win11-desktop/nvim/lua/plugins/treesitter.lua
+++ win11-desktop/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,64 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		branch = "master",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = { "lua", "python", "javascript", "c", "cpp", "rust", "zig" },
-				sync_install = false,
-				auto_install = true,
-				highlight = {
-					enable = true,
-				},
-				additional_vim_regex_highlighting = false,
-				indent = {
-					enable = true,
-					disable = { "rust", "c" },
-				},
-			})
-		end,
-	},
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local ts = require("nvim-treesitter")
+		local parsers = {
+			"bash",
+			"comment",
+			"css",
+			"diff",
+			"git_config",
+			"gitcommit",
+			"gitignore",
+			"groovy",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"json5",
+			"lua",
+			"make",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"regex",
+			"rust",
+			"ssh_config",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		}
+
+		for _, parser in ipairs(parsers) do
+			ts.install(parser)
+		end
+
+		-- Not every tree-sitter parser is the same as the file type detected
+		-- So the patterns need to be registered more cleverly
+		local patterns = {}
+		for _, parser in ipairs(parsers) do
+			local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+			for _, pp in pairs(parser_patterns) do
+				table.insert(patterns, pp)
+			end
+		end
+
+		vim.treesitter.language.register("groovy", "Jenkinsfile")
+		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo[0][0].foldmethod = "expr"
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = patterns,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
+
+-- Thanks to https://mhpark.me/posts/update-treesitter-main/
